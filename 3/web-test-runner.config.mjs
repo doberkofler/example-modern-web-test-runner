d011a912db3fd214e1cec894119a958fd69c58f0
Dieter Oberkofler <1009585+doberkofler@users.noreply.github.com>
2025-06-19 07:07:07 +0200

    Add playwright

--- 3/web-test-runner.config.mjs
+++ 3/web-test-runner.config.mjs
@@ -1,6 +1,13 @@
 import {esbuildPlugin} from '@web/dev-server-esbuild';
+import {playwrightLauncher} from '@web/test-runner-playwright';
 
 export default {
+	files: 'src/**/*.test.ts',
 	nodeResolve: true,
 	plugins: [esbuildPlugin({ts: true})],
+	browsers: [
+		playwrightLauncher({product: 'chromium'}),
+		playwrightLauncher({product: 'firefox'}),
+		playwrightLauncher({product: 'webkit'}),
+	],
 };
