989d3b251c72813d6e29725684df7263d8b8e385
Dieter Oberkofler <1009585+doberkofler@users.noreply.github.com>
2025-06-19 12:26:45 +0200

    Add lint

---
+++ 4/test/test-helpers.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+export function fixture(jsx) {
+    const wrapper = document.createElement('div');
+    ReactDOM.render(jsx, wrapper);
+    return {
+        element: wrapper.firstElementChild,
+        restoreFixture: () => wrapper.remove(),
+    };
+}
