31711cab8c04f5601b5b0054d975b5bdcdaadec9
Dieter Oberkofler <1009585+doberkofler@users.noreply.github.com>
2025-06-20 06:35:17 +0200

    Cleanup

--- 4/test/test-helpers.jsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom';
-export function fixture(jsx) {
-    const wrapper = document.createElement('div');
-    ReactDOM.render(jsx, wrapper);
-    return {
-        element: wrapper.firstElementChild,
-        restoreFixture: () => wrapper.remove(),
-    };
-}
